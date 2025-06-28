1cddf6a0d2b26951f31b13cb45f7d9b3e875b20a
50685 <wenacu98@gmail.com>
2025-06-28 00:24:31 -0600

    Proyecto 107

---
+++ src/test/java/automation/NavegationTests.java
@@ -0,0 +1,43 @@
+package automation;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.BaseTest;
+import utilities.Logs;
+
+public class NavegationTests extends BaseTest {
+    @Test
+    public void ejercicio1Test() {
+        final var url = "https://www.saucedemo.com/";
+
+        Logs.info("Navegando a: %s", url);
+        driver.get(url);
+
+        Logs.info("Obteniendo la URL actual");
+        final var currentUrl = driver.getCurrentUrl();
+
+        Logs.info("Verificando que las urls sean iguales");
+        Assert.assertEquals(currentUrl, url);
+    }
+
+    @Test
+    public void ejercicio2Test() {
+        final var urlHeroku = "https://the-internet.herokuapp.com/";
+        final var urlGithub = "https://github.com/";
+
+        Logs.info("Navegando a: %s", urlHeroku);
+        driver.get(urlHeroku);
+
+        Logs.info("Navegando a: %s", urlGithub);
+        driver.get(urlGithub);
+
+        Logs.info("Regresando a la página anterior");
+        driver.navigate().back();
+
+        Logs.info("Obteniendo la URL actual");
+        final var currentUrl = driver.getCurrentUrl();
+
+        Logs.info("Verificando que las urls sean iguales");
+        Assert.assertEquals(currentUrl, urlHeroku);
+    }
+}
